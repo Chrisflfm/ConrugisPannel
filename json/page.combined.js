5b5d99f03831b3d9e5dc1864221cd2f098eb71a3
Chrisflfm <chris.dries@skynet.be>
2020-09-15 09:52:55 +0200

    plattegronden omgedraait

--- json/page.combined.js
+++ json/page.combined.js
@@ -1,1 +1,64 @@
-$hmi.addPagesScript({});
+$hmi.addPagesScript({// ------------------------------------  camera  ------------------------------ 
+
+"m5_page5": function(){
+// ------------------------------------  Header ------------------------------
+var $=null;var jQuery=null;
+$hmi.getActivePage().__eval__ = function(a){return eval(a);}
+var project = $hmi.getProjectInterface();
+var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
+page.__eval__=$hmi.getActivePage().__eval__;
+var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
+// --------------------------------------User code -------------------------------
+// ------------------------------------  Footer  ------------------------------
+page.bindJSCallback(false);
+
+}
+,
+// ------------------------------------  Settings  ------------------------------ 
+
+"m6_page6": function(){
+// ------------------------------------  Header ------------------------------
+var $=null;var jQuery=null;
+$hmi.getActivePage().__eval__ = function(a){return eval(a);}
+var project = $hmi.getProjectInterface();
+var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
+page.__eval__=$hmi.getActivePage().__eval__;
+var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
+// --------------------------------------User code -------------------------------
+// ------------------------------------  Footer  ------------------------------
+page.bindJSCallback(false);
+
+}
+,
+// ------------------------------------  SettingsInfo  ------------------------------ 
+
+"m7_page7": function(){
+// ------------------------------------  Header ------------------------------
+var $=null;var jQuery=null;
+$hmi.getActivePage().__eval__ = function(a){return eval(a);}
+var project = $hmi.getProjectInterface();
+var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
+page.__eval__=$hmi.getActivePage().__eval__;
+var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
+// --------------------------------------User code -------------------------------
+// ------------------------------------  Footer  ------------------------------
+page.bindJSCallback(false);
+
+}
+,
+// ------------------------------------  Weerbericht  ------------------------------ 
+
+"m8_page8": function(){
+// ------------------------------------  Header ------------------------------
+var $=null;var jQuery=null;
+$hmi.getActivePage().__eval__ = function(a){return eval(a);}
+var project = $hmi.getProjectInterface();
+var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
+page.__eval__=$hmi.getActivePage().__eval__;
+var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
+// --------------------------------------User code -------------------------------
+// ------------------------------------  Footer  ------------------------------
+page.bindJSCallback(false);
+
+}
+});
