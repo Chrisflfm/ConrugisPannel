a598b180c6b0a824c20f66e6dd5cf686045fcd0e
Chrisflfm <chris.dries@skynet.be>
2020-10-24 10:11:10 +0200

    met modbus operationeel

--- json/page.combined.js
+++ json/page.combined.js
@@ -1,16 +1,1 @@
-$hmi.addPagesScript({// ------------------------------------  Verwarming  ------------------------------ 
-
-"m2_page2": function(){
-// ------------------------------------  Header ------------------------------
-var $=null;var jQuery=null;
-$hmi.getActivePage().__eval__ = function(a){return eval(a);}
-var project = $hmi.getProjectInterface();
-var page = new $hmi.defs.hmiIPage($hmi.getActivePage());
-page.__eval__=$hmi.getActivePage().__eval__;
-var Group = $hmi.getJSModule('Group');var State = $hmi.getJSModule('State');
-// --------------------------------------User code -------------------------------
-// ------------------------------------  Footer  ------------------------------
-page.bindJSCallback(false);
-
-}
-});
+$hmi.addPagesScript({});
